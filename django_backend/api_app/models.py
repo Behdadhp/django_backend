6118737adedeae620f3ccfed40eb77914835df5d
behdadhp <behdad.hajipour95@gmail.com>
2022-05-17 18:33:26 +0200

    added Branch model

--- django_backend/api_app/models.py
+++ django_backend/api_app/models.py
@@ -6,9 +6,17 @@ from datetime import datetime
 
 class Vacation(models.Model):
 
-    start_date = models.DateField(default=datetime.now,blank=False,Null=False)
+    start_date = models.DateField(default=datetime.now,blank=False,null=False)
     end_date = models.DateField(blank=False,Null=False)
 
     def __str__(self):
         return self.start_date.strftime('%Y,%m,%d')
 
+
+class Branch(models.Model):
+
+    location = CharField(max_length= 128, blank=False,null=False)
+    name = CharField(max_length= 128, blank=False,null=False)
+
+    def __str__(self):
+        return self.name
