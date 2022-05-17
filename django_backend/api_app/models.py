7d2d883d291e4212a97ee72c6d1c6a68b100f308
behdadhp <behdad.hajipour95@gmail.com>
2022-05-17 18:32:01 +0200

    added Vacation models

--- django_backend/api_app/models.py
+++ django_backend/api_app/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from datetime import datetime
 
 # Create your models here.
+
+
+class Vacation(models.Model):
+
+    start_date = models.DateField(default=datetime.now,blank=False,Null=False)
+    end_date = models.DateField(blank=False,Null=False)
+
+    def __str__(self):
+        return self.start_date.strftime('%Y,%m,%d')
+
