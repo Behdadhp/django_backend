32a63c13398309708ba87cf49d1010855d7fd725
behdadhp <behdad.hajipour95@gmail.com>
2022-05-17 19:20:31 +0200

    edited views.py with VacationForm

--- django_backend/api_app/views.py
+++ django_backend/api_app/views.py
@@ -1,12 +1,13 @@
 from django.shortcuts import render
 from .models import Vacation
 from django.views import generic
+from .forms import VacationForm
 
 # Create your views here.
 
 class VacationCreate(generic.CreateView):
     model = Vacation
-    form_class = CreateVacation
+    form_class = VacationForm
 
     def form_valid(self,form):
         self.object = form.save(commit=False)
