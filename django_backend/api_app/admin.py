1bea632b245ff3c37908a41d441ac07a7cb76191
behdadhp <behdad.hajipour95@gmail.com>
2022-05-17 18:47:02 +0200

    created admin.py

--- django_backend/api_app/admin.py
+++ django_backend/api_app/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Vacation,Branch,Employee
 
-# Register your models here.
+
+admin.site.register(Vacation)
+admin.site.register(Branch)
+admin.site.register(Employee)
+
