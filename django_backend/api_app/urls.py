7905350b0a60af256ccb2b55c60051c1612cd1d8
behdadhp <behdad.hajipour95@gmail.com>
2022-05-17 20:55:09 +0200

    updated urls.py with viewset VacationViewSet

--- django_backend/api_app/urls.py
+++ django_backend/api_app/urls.py
@@ -1,10 +1,15 @@
 from django.urls import path
 from api_app import views
+from .views import VacationViewSet
+from rest_framework.routers import DefaultRouter
 
+router = DefaultRouter()
+router.register('vacation', VacationViewSet,basename='vacation')
 
 app_name = 'vacation'
 
 urlpatterns=[
     path('',views.VacationCreate.as_view(), name='create'),
-    path('vacation_list',views.VacationList.as_view(),name='list')
+    path('vacation_list',views.VacationList.as_view(),name='list'),
+    path('viewset/',include(router.urls)),
 ]
